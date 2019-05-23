11258ee74a825cfe7d2b793192ac32d6f620ea3c
jarrensj <sanjose.jarren@gmail.com>
2019-05-22 21:55:12 -0700

    clean up and initial components

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Boba Shop</h1>
+          <p className="lead">
+            Create an account to be a part of the community and earn rewards!
+          </p>
+          <div className="buttons">
+            <a href="register.html" className="btn btn-primary">Sign Up</a>
+            <a href="login.html" className="btn btn-light">Login</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Landing;
