6b5fced48841888891249259cad50faccad354ad
jarrensj <sanjose.jarren@gmail.com>
2019-05-22 22:31:21 -0700

    react router

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar bg-dark">
       <h1>
-        <a href="index.html"><i class="fas fa-coffee"></i> Boba Shop</a>
+        <Link to="/"><i class="fas fa-coffee"></i> Boba Shop</Link>
       </h1>
       <ul>
-        <li><a href="register.html">Register</a></li>
-        <li><a href="login.html">Login</a></li>
+        <li><Link to="/register">Register</Link></li>
+        <li><Link to="/login">Login</Link></li>
       </ul>
     </nav>
   )
